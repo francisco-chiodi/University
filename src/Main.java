f976ce8abc0147d5d78c3d27ea29de447c97fa10
francisco-chiodi <franchiodi98@gmail.com>
2025-08-25 18:51:44 -0400

    javaChapter

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,73 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public class Example{
+        public int x;
+        public int a;
+
+        public void makingSomething(){
+            x=1 + a *3;
+        }
+
+        public int sum(){
+            return x + a;
+        }
+
+        public void print{
+
+        }
+
+    }
 }
+
+/*
+Syntax methods
+
+modifiers: public/private
+
+public TypeReturnValue MethodName (parametersList) {
+}
+private TypeReturnValue MethodName (parametersList) {
+}
+public TypeReturnValue MethodName (parametersList) {
+}
+private TyperReturnValue MethodName (parametersList) {
+}
+public TyperReturnValue MethodName (parametersList) {
+}
+private TyperReturnValue MethodName(parametersList) {
+}
+
+Primitive types methods
+
+public byte MethodName(parametersList){
+}
+
+private byte MethodName() {
+} //no parameter
+
+public byte methodName(int x) {
+}
+
+private byte methodName(String s, double d) {
+}
+
+public byte methodName(int[] arr) {
+}
+
+private byte methodName(MyClass obj){
+}
+
+Reference method types
+Reference method types means that the method returns an object
+
+public class methodName(String) {
+}
+
+
+
+
+
+
+ */
